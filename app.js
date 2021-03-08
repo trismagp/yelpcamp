e3cda7ff25dfed535283473527098a24d5cc0f25
Tristan <tristan@klis-mbp.home>
2021-03-08 23:54:24 +0100

    create campground model insert one record

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const Campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp',{
     useNewUrlParser: true,
@@ -20,6 +21,12 @@ app.set('views', path.join(__dirname,'views'));
 
 app.get('/', (req, res) => {
     res.render('home')
+})
+
+app.get('/makecampground', async (req, res) => {
+    const camp = new Campground({title: "camp01" ,description: "very fund"});
+    await camp.save();
+    res.send(camp);
 })
 
 app.listen(3000, ()=>{
