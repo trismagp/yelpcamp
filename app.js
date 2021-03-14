4c46d3cad43fd6ecc19174492e69790e5b1c3b16
Tristan <tristan@klis-mbp.home>
2021-03-14 20:51:16 +0100

    add campgrounds show

--- app.js
+++ app.js
@@ -28,6 +28,12 @@ app.get('/campgrounds', async (req, res) => {
     res.render('campgrounds/index', {campgrounds});
 })
 
+app.get('/campgrounds/:id', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show', {campground});
+})
+
+
 app.get('/makecampground', async (req, res) => {
     const camp = new Campground({title: "camp01" ,description: "very fund"});
     await camp.save();
