d62fddd1c8160a422f136dceaf03b4e4ebdfe989
Tristan <tristan@klis-mbp.home>
2021-03-15 08:48:18 +0100

    add campgrounds new and create

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ const app = express();
 app.set('view engine','ejs');
 app.set('views', path.join(__dirname,'views'));
 
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
     res.render('home')
 })
@@ -26,6 +28,16 @@ app.get('/', (req, res) => {
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', {campgrounds});
+})
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+})
+
+app.post('/campgrounds', async (req, res) => {
+    const camp = new Campground(req.body.campground);
+    await camp.save();
+    res.redirect(`/campgrounds/${camp._id}`);
 })
 
 app.get('/campgrounds/:id', async (req, res) => {
